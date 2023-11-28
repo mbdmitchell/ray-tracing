8e2fedd0b5f5e8771b20308e4f5275b6520fd35e
Max Mitchell <maxmitchell.mm@googlemail.com>
2023-11-28 16:52:05 +0000

    Change Camera class access modifiers

--- Camera.h
+++ Camera.h
@@ -2,7 +2,12 @@
 
 #include "Vec3.h"
 
-struct Camera {
-    Vec3 center;
-    double focal_length;
+class Camera {
+    Point3 m_center;
+    double m_focal_length;
+public:
+    Camera(Point3 center, double focal_length) : m_center{center}, m_focal_length{focal_length} { }
+    auto center() const {return m_center; }
+    auto focal_length() const {return m_focal_length; }
+
 };
--- Viewport.cpp
+++ Viewport.cpp
@@ -3,7 +3,7 @@
 Viewport::Viewport(const ImageDimensions &dim, const Camera &cam) :
         m_height {2.0}  // Hard-coded for now
         , m_width {m_height * (static_cast<double>(dim.width) / gsl::narrow_cast<int>(dim.height))}
-        , m_upper_left {cam.center - Vec3{0, 0, cam.focal_length} - Vec3{m_width, 0, 0} / 2 - Vec3{0, -m_height, 0} / 2}
+        , m_upper_left {cam.center() - Vec3{0, 0, cam.focal_length()} - Vec3{m_width, 0, 0} / 2 - Vec3{0, -m_height, 0} / 2}
         , m_horizontal {m_width,0,0}
         , m_vertical {0,-m_height,0}
         , m_horizontalPixelDelta {m_horizontal / gsl::narrow_cast<int>(dim.width)}
