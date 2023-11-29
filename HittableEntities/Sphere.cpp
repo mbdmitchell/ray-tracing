024931e719e603711dc558da1731833acf477478
Max Mitchell <maxmitchell.mm@googlemail.com>
2023-11-29 15:34:16 +0000

    Remove unused ctor

--- HittableEntities/Sphere.cpp
+++ HittableEntities/Sphere.cpp
@@ -1,10 +1,5 @@
 #include "Sphere.h"
 
-Sphere::Sphere(double radius, const Point3 &center) : m_radius{radius}, m_center{center} {
-    if (m_radius <= 0) {
-        throw std::invalid_argument("radius must be >0");
-    }
-}
 
 double Sphere::radius() const { return m_radius; }
 
